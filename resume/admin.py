0fb35832287e3097084151f6a46291756a88c9a3
J.T. Robinson <jaittarius@hotmail.com>
2017-12-29 16:34:49 -0500

    Added resume app and preliminary Resume model

--- resume/admin.py
+++ resume/admin.py
@@ -3,4 +3,21 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Resume, Skills, Experience, Education
+
+class SkillsInline(admin.StackedInline):
+    model = Skills
+
+
+class ExperienceInline(admin.StackedInline):
+    model = Experience
+
+
+class EducationInline(admin.StackedInline):
+    model = Education
+
+class ResumeAdmin(admin.ModelAdmin):
+    inlines = [SkillsInline, ExperienceInline, EducationInline]
+
 # Register your models here.
+admin.site.register(Resume, ResumeAdmin)
--- resume/models.py
+++ resume/models.py
@@ -4,3 +4,27 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Resume(models.Model):
+    title = models.CharField(max_length = 30)
+    description = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
+
+class Skills(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    skill_name = models.CharField(max_length = 30)
+
+class Experience(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    job_title = models.CharField(max_length = 30)
+    start_year = models.IntegerField()
+    end_year = models.IntegerField()
+
+class Education(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+    school_name = models.CharField(max_length = 30)
+    city = models.CharField(max_length = 30)
+    state = models.CharField(max_length = 2)
+    year = models.IntegerField()
